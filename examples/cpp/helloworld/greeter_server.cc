c65aaf2e44d4884ed00464bad49f0811e2178c21
Esun Kim <veblush@google.com>
2021-01-06 14:20:49 -0800

    Added test

--- examples/cpp/helloworld/greeter_server.cc
+++ examples/cpp/helloworld/greeter_server.cc
@@ -1,76 +1,200 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-#include <iostream>
-#include <memory>
-#include <string>
-
+#include <experimental/optional>
+#include <grpcpp/generic/generic_stub.h>
 #include <grpcpp/grpcpp.h>
-#include <grpcpp/health_check_service_interface.h>
-#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/impl/codegen/async_generic_service.h>
+#include <thread>
+#include <unordered_map>
+#include "absl/types/optional.h"
 
-#ifdef BAZEL_BUILD
-#include "examples/protos/helloworld.grpc.pb.h"
-#else
-#include "helloworld.grpc.pb.h"
-#endif
+#define DEBUG(x)                                                        \
+  do {                                                                  \
+    ::std::cerr << __FILE__ ":" << __LINE__ << ": " << x << ::std::endl; \
+  } while (false)
 
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::Status;
-using helloworld::HelloRequest;
-using helloworld::HelloReply;
-using helloworld::Greeter;
+struct Server {
+  void Start() {
+    grpc::ServerBuilder builder;
+    builder.RegisterAsyncGenericService(&service_);
+    builder.AddListeningPort("0.0.0.0:0", grpc::InsecureServerCredentials(), &port_);
+    cq_ = builder.AddCompletionQueue();
+    server_ = builder.BuildAndStart();
+    worker_ = std::thread(&Server::HandleRpcs, this);
+  }
 
-// Logic and data behind the server's behavior.
-class GreeterServiceImpl final : public Greeter::Service {
-  Status SayHello(ServerContext* context, const HelloRequest* request,
-                  HelloReply* reply) override {
-    std::string prefix("Hello ");
-    reply->set_message(prefix + request->name());
-    return Status::OK;
+  void Shutdown() {
+    stopping_ = true;
+    server_->Shutdown();
+    cq_->Shutdown();
+    worker_.join();
+  }
+
+  int Port() const { return port_; }
+
+private:
+  struct CallData;
+  struct Tag;
+
+  grpc::AsyncGenericService service_;
+  std::unique_ptr<grpc::Server> server_;
+  std::unique_ptr<grpc::ServerCompletionQueue> cq_;
+  int port_;
+  std::thread worker_;
+  std::unordered_map<CallData*, std::shared_ptr<CallData>> data_;
+  std::mutex data_mut_;
+  std::atomic_bool stopping_{};
+
+  void HandleRpcs();
+
+  void NewData() {
+    if (stopping_) return;
+    auto d = std::make_shared<CallData>(*this);
+    std::lock_guard<std::mutex> lk(data_mut_);
+    data_.emplace(d.get(), std::move(d));
+  }
+
+  void Remove(CallData* p) {
+    std::lock_guard<std::mutex> lk(data_mut_);
+    auto it = data_.find(p);
+    if (it != data_.end()) data_.erase(it);
   }
 };
 
-void RunServer() {
-  std::string server_address("0.0.0.0:50051");
-  GreeterServiceImpl service;
+struct Server::Tag {
+  void* tag;
+  bool Done() const { return reinterpret_cast<uintptr_t>(tag)&1; }
+  void* MarkDone() const { return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(tag)|1); }
+  CallData* AsCallData() const { return reinterpret_cast<CallData*>(reinterpret_cast<uintptr_t>(tag)&~1); }
+};
 
-  grpc::EnableDefaultHealthCheckService(true);
-  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
-  ServerBuilder builder;
-  // Listen on the given address without any authentication mechanism.
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
-  builder.RegisterService(&service);
-  // Finally assemble the server.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
+struct Server::CallData {
+  CallData(Server& server): responder_(&ctx_), server_(server) {
+    Tag tag{this};
+    assert(!tag.Done());
+    ctx_.AsyncNotifyWhenDone(tag.MarkDone());
+    server_.service_.RequestCall(&ctx_, &responder_, server_.cq_.get(), server_.cq_.get(), tag.AsCallData());
+  }
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
-  server->Wait();
+  void Proceed() {
+    switch (state_) {
+    case kCreate:
+      server_.NewData();
+      state_ = kWrite;
+      responder_.Read(&request_, this);
+      break;
+    case kWrite:
+      if (write_count_ < 10) {
+        ++write_count_;
+        responder_.Write(request_, this);
+      } else {
+        state_ = kFinish;
+        responder_.Finish(grpc::Status::OK, this);
+      }
+      break;
+    case kFinish:
+      server_.Remove(this);
+      break;
+    }
+  }
+
+private:
+  grpc::GenericServerContext ctx_;
+  grpc::GenericServerAsyncReaderWriter responder_;
+  Server& server_;
+  enum { kCreate, kWrite, kFinish } state_ = kCreate;
+  grpc::ByteBuffer request_;
+  int write_count_ = 0;
+};
+
+void Server::HandleRpcs() {
+  NewData();
+  for (;;) {
+    Tag tag;
+    bool ok;
+    if (!cq_->Next(&tag.tag, &ok)) break;
+    if (!ok || tag.Done()) {
+      Remove(tag.AsCallData());
+    } else {
+      std::shared_ptr<CallData> p;
+      {
+        std::lock_guard<std::mutex> lk(data_mut_);
+        auto it = data_.find(tag.AsCallData());
+        if (it == data_.end()) continue;
+        p = it->second;
+      }
+      p->Proceed();
+    }
+  }
 }
 
-int main(int argc, char** argv) {
-  RunServer();
+struct ClientCallData {
+  ClientCallData(grpc::GenericStub& stub, grpc::CompletionQueue* cq, uintptr_t tag)
+    : cq_(cq), tag_(reinterpret_cast<void*>(tag)) {
+    grpc::Slice s(std::string(1<<20, 'a'));
+    request_ = grpc::ByteBuffer(&s, 1);
+    ctx_.set_deadline(std::chrono::system_clock::now() + std::chrono::seconds(10));
+    pipe_ = stub.PrepareCall(&ctx_, "/test", cq_);
+    pipe_->StartCall(tag_);
+  }
 
+  absl::optional<grpc::Status> Proceed(bool ok) {
+    DEBUG((uintptr_t)tag_ << '\t' << state_ << '\t' << ok);
+    switch (state_) {
+    case kStart:
+      if (!ok) return grpc::Status(grpc::UNKNOWN, "channel broken");
+      state_ = kRead;
+      pipe_->Write(request_, tag_);
+      return absl::nullopt;
+    case kRead:
+      if (ok) {
+        pipe_->Read(&response_, tag_);
+      } else {
+        state_ = kFinish;
+        pipe_->Finish(&status_, tag_);
+      }
+      return absl::nullopt;
+    case kFinish:
+      return status_;
+    }
+  }
+
+private:
+  grpc::CompletionQueue* cq_;
+  void* tag_;
+  grpc::ClientContext ctx_;
+  std::unique_ptr<grpc::ClientAsyncReaderWriter<grpc::ByteBuffer, grpc::ByteBuffer>> pipe_;
+  enum { kStart, kRead, kFinish } state_ = kStart;
+  grpc::ByteBuffer request_, response_;
+  grpc::Status status_;
+};
+
+int main() {
+  Server server;
+  server.Start();
+  DEBUG("Server listening on port " << server.Port());
+  grpc::GenericStub stub(grpc::CreateChannel("localhost:" + std::to_string(server.Port()),
+                                             grpc::InsecureChannelCredentials()));
+  grpc::CompletionQueue cq;
+  std::vector<std::unique_ptr<ClientCallData>> calls(10);
+  for (unsigned i = 0; i < calls.size(); ++i) {
+    calls[i] = absl::make_unique<ClientCallData>(stub, &cq, i);
+  }
+  int pending = calls.size();
+  std::vector<bool> done(calls.size());
+  for (;;) {
+    void* tag;
+    bool ok;
+    if (!cq.Next(&tag, &ok)) break;
+    int i = reinterpret_cast<uintptr_t>(tag);
+    if (auto s = calls[i]->Proceed(ok)) {
+      if (!s->ok()) {
+        DEBUG(s->error_code() << ' ' << s->error_message());
+      }
+      if (!done[i]) {
+        done[i] = true;
+        if (--pending == 0) break;
+      }
+    }
+  }
+  server.Shutdown();
   return 0;
 }
