0544a8993e4f03a875038c15908ff04ead29a529
Esun Kim <veblush@google.com>
2021-09-16 12:48:23 -0700

    Add absl::Status support to closure

--- src/core/lib/iomgr/combiner.cc
+++ src/core/lib/iomgr/combiner.cc
@@ -149,7 +149,7 @@ static void combiner_exec(grpc_core::Combiner* lock, grpc_closure* cl,
   }
   GPR_ASSERT(last & STATE_UNORPHANED);  // ensure lock has not been destroyed
   assert(cl->cb);
-  cl->error_data.error = error;
+  *cl->error_data.error = error;
   lock->queue.Push(cl->next_data.mpscq_node.get());
 }
 
@@ -221,11 +221,13 @@ bool grpc_combiner_continue_exec_ctx() {
       return true;
     }
     grpc_closure* cl = reinterpret_cast<grpc_closure*>(n);
-    grpc_error_handle cl_err = cl->error_data.error;
+    grpc_error_handle cl_err = std::move(*cl->error_data.error);
+    cl->error_data.error.Destroy();
+    cl->error_data.error.Init();
 #ifndef NDEBUG
     cl->scheduled = false;
 #endif
-    cl->cb(cl->cb_arg, cl_err);
+    cl->cb(cl->cb_arg, std::move(cl_err));
     GRPC_ERROR_UNREF(cl_err);
   } else {
     grpc_closure* c = lock->final_list.head;
@@ -236,11 +238,13 @@ bool grpc_combiner_continue_exec_ctx() {
       GRPC_COMBINER_TRACE(
           gpr_log(GPR_INFO, "C:%p execute_final[%d] c=%p", lock, loops, c));
       grpc_closure* next = c->next_data.next;
-      grpc_error_handle error = c->error_data.error;
+      grpc_error_handle error = std::move(*c->error_data.error);
+      c->error_data.error.Destroy();
+      c->error_data.error.Init();
 #ifndef NDEBUG
       c->scheduled = false;
 #endif
-      c->cb(c->cb_arg, error);
+      c->cb(c->cb_arg, std::move(error));
       GRPC_ERROR_UNREF(error);
       c = next;
     }
