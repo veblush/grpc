5103b0794ce974ca9d55387a8db1e61922a5adb1
Esun Kim <veblush@google.com>
2021-09-16 12:48:23 -0700

    Add absl::Status support to closure

--- src/core/lib/iomgr/call_combiner.cc
+++ src/core/lib/iomgr/call_combiner.cc
@@ -142,7 +142,7 @@ void CallCombiner::Start(grpc_closure* closure, grpc_error_handle error,
       gpr_log(GPR_INFO, "  QUEUING");
     }
     // Queue was not empty, so add closure to queue.
-    closure->error_data.error = error;
+    *closure->error_data.error = error;
     queue_.Push(
         reinterpret_cast<MultiProducerSingleConsumerQueue::Node*>(closure));
   }
@@ -180,9 +180,11 @@ void CallCombiner::Stop(DEBUG_ARGS const char* reason) {
       if (GRPC_TRACE_FLAG_ENABLED(grpc_call_combiner_trace)) {
         gpr_log(GPR_INFO, "  EXECUTING FROM QUEUE: closure=%p error=%s",
                 closure,
-                grpc_error_std_string(closure->error_data.error).c_str());
+                grpc_error_std_string(*closure->error_data.error).c_str());
       }
-      ScheduleClosure(closure, closure->error_data.error);
+      grpc_error_handle error = *closure->error_data.error;
+      *closure->error_data.error = GRPC_ERROR_NONE;
+      ScheduleClosure(closure, error);
       break;
     }
   } else if (GRPC_TRACE_FLAG_ENABLED(grpc_call_combiner_trace)) {
