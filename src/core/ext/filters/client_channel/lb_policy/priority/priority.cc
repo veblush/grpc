f44a919e605762565f05b0df2eb0a40d00d62e99
veblush <veblush@users.noreply.github.com>
2022-03-31 22:52:08 +0000

    Automated change: Fix sanity tests

--- src/core/ext/filters/client_channel/lb_policy/priority/priority.cc
+++ src/core/ext/filters/client_channel/lb_policy/priority/priority.cc
@@ -443,8 +443,8 @@ void PriorityLb::TryNextPriorityLocked(bool report_connecting) {
     }
     // DEBUG
     if (priority == config_->priorities().size() - 1) {
-      gpr_log(GPR_ERROR, "[priority_lb %p] ugh last priority %u, child %s", this,
-              priority, child_name.c_str());
+      gpr_log(GPR_ERROR, "[priority_lb %p] ugh last priority %u, child %s",
+              this, priority, child_name.c_str());
       GPR_ASSERT(false);
     }
     // DEBUG
