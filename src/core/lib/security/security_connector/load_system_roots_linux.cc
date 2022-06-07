ec18fad661bd826ffbe7b2c7c5d0069c1adf284c
Esun Kim <veblush@google.com>
2022-06-07 10:46:50 -0700

    Convert

--- src/core/lib/security/security_connector/load_system_roots_linux.cc
+++ src/core/lib/security/security_connector/load_system_roots_linux.cc
@@ -65,7 +65,7 @@ grpc_slice GetSystemRootCerts() {
   for (size_t i = 0; i < num_cert_files_; i++) {
     grpc_error_handle error =
         grpc_load_file(kLinuxCertFiles[i], 1, &valid_bundle_slice);
-    if (error == GRPC_ERROR_NONE) {
+    if (GRPC_ERROR_IS_NONE(error)) {
       return valid_bundle_slice;
     } else {
       GRPC_ERROR_UNREF(error);
