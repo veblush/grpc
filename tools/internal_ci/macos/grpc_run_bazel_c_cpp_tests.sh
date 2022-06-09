afbd3b7493bb72fc99bdfa7657b6ba60d73ba659
Esun Kim <veblush@google.com>
2022-06-09 15:51:39 -0700

    h2_local_uds_percent_encoded_test@grpc_authz only

--- tools/internal_ci/macos/grpc_run_bazel_c_cpp_tests.sh
+++ tools/internal_ci/macos/grpc_run_bazel_c_cpp_tests.sh
@@ -49,4 +49,5 @@ bazel_c_cpp_tests/bazel_wrapper \
   --google_credentials="${KOKORO_GFILE_DIR}/GrpcTesting-d0eeee2db331.json" \
   "${BAZEL_REMOTE_CACHE_ARGS[@]}" \
   $BAZEL_FLAGS \
-  -- //test/...
+  --runs_per_test=1000 \
+  -- //test/core/end2end:h2_local_uds_percent_encoded_test@grpc_authz
