c7ba531db1ade59a959891f0bec037fb5e059127
Esun Kim <veblush@google.com>
2022-06-15 16:49:31 -0700

    Log to FileWatcherAuthorizationPolicyProvider

--- test/core/end2end/tests/grpc_authz.cc
+++ test/core/end2end/tests/grpc_authz.cc
@@ -55,6 +55,7 @@ static void wait_for_policy_reload(void) {
   // Wait for the provider's refresh thread to read the updated files.
   // TODO(jtattermusch): Refactor the tests to use a more reliable mechanism of
   // detecting that the policy has been reloaded. See b/204329811
+  gpr_log(GPR_INFO, "wait_for_policy_reload");
   gpr_sleep_until(grpc_timeout_seconds_to_deadline(5));
 }
 
