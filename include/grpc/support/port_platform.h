11980919c25eb8a35e109b83ea1c928e7f217722
Esun Kim <veblush@google.com>
2023-03-31 10:59:28 -0700

    Enabled GRPC_CORD_SUPPORT_ENABLED

--- include/grpc/support/port_platform.h
+++ include/grpc/support/port_platform.h
@@ -19,6 +19,8 @@
 #ifndef GRPC_SUPPORT_PORT_PLATFORM_H
 #define GRPC_SUPPORT_PORT_PLATFORM_H
 
+#define GRPC_CORD_SUPPORT_ENABLED 1
+
 /*
  * Define GPR_BACKWARDS_COMPATIBILITY_MODE to try harder to be ABI
  * compatible with older platforms (currently only on Linux)
