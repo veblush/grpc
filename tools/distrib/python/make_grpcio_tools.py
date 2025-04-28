8d342df4430a3baad0f0232980bb2d53df25510b
Esun Kim <veblush@google.com>
2025-04-28 09:56:10 -0700

    Added upb-gen/google/protobuf to python/make_grpcio_tools

--- tools/distrib/python/make_grpcio_tools.py
+++ tools/distrib/python/make_grpcio_tools.py
@@ -70,6 +70,7 @@ PROTOBUF_PROTO_PREFIX = "@com_google_protobuf//src/"
 
 # will be added to include path when building grpcio_tools
 CC_INCLUDES = [
+    os.path.join("core", "ext", "upb-gen"),
     os.path.join("third_party", "abseil-cpp"),
     os.path.join("third_party", "protobuf"),
     os.path.join("third_party", "protobuf", "src"),
@@ -88,6 +89,7 @@ GRPCIO_TOOLS_ROOT_PREFIX = "tools/distrib/python/grpcio_tools/"
 COPY_FILES_SOURCE_TARGET_PAIRS = [
     ("include", "grpc_root/include"),
     ("src/compiler", "grpc_root/src/compiler"),
+    ("src/core/ext/upb-gen/google/protobuf", "grpc_root/core/ext/upb-gen/google/protobuf"),
     ("third_party/abseil-cpp/absl", "third_party/abseil-cpp/absl"),
     ("third_party/protobuf/src", "third_party/protobuf/src"),
     ("third_party/protobuf/upb", "third_party/protobuf/upb"),
