240e870095c3603560357156a0c7f90c1c8cefe4
veblush <2793282+veblush@users.noreply.github.com>
2025-04-28 22:19:25 +0000

    Automated change: Fix sanity tests

--- tools/distrib/python/grpcio_tools/protoc_lib_deps.py
+++ tools/distrib/python/grpcio_tools/protoc_lib_deps.py
@@ -346,6 +346,7 @@ CC_FILES=[
     'third_party/protobuf/upb/mini_table/extension_registry.c',
     'third_party/protobuf/upb/mini_table/internal/message.c',
     'third_party/protobuf/upb/mini_table/message.c',
+    'third_party/protobuf/upb/reflection/cmake/google/protobuf/descriptor.upb_minitable.c',
     'third_party/protobuf/upb/reflection/def_pool.c',
     'third_party/protobuf/upb/reflection/def_type.c',
     'third_party/protobuf/upb/reflection/desc_state.c',
@@ -364,7 +365,6 @@ CC_FILES=[
     'third_party/protobuf/upb/reflection/oneof_def.c',
     'third_party/protobuf/upb/reflection/service_def.c',
     'third_party/protobuf/upb/reflection/stage0/google/protobuf/descriptor.upb.c',
-    'third_party/protobuf/upb/reflection/stage1/google/protobuf/descriptor.upb_minitable.c',
     'third_party/protobuf/upb/wire/decode.c',
     'third_party/protobuf/upb/wire/encode.c',
     'third_party/protobuf/upb/wire/eps_copy_input_stream.c',
@@ -373,6 +373,7 @@ CC_FILES=[
     'third_party/protobuf/upb_generator/c/generator.cc',
     'third_party/protobuf/upb_generator/c/names.cc',
     'third_party/protobuf/upb_generator/c/names_internal.cc',
+    'third_party/protobuf/upb_generator/cmake/google/protobuf/compiler/plugin.upb_minitable.c',
     'third_party/protobuf/upb_generator/common.cc',
     'third_party/protobuf/upb_generator/common/names.cc',
     'third_party/protobuf/upb_generator/file_layout.cc',
@@ -382,8 +383,7 @@ CC_FILES=[
     'third_party/protobuf/upb_generator/minitable/names.cc',
     'third_party/protobuf/upb_generator/minitable/names_internal.cc',
     'third_party/protobuf/upb_generator/plugin.cc',
-    'third_party/protobuf/upb_generator/stage0/google/protobuf/compiler/plugin.upb.c',
-    'third_party/protobuf/upb_generator/stage1/google/protobuf/compiler/plugin.upb_minitable.c'
+    'third_party/protobuf/upb_generator/stage0/google/protobuf/compiler/plugin.upb.c'
 ]
 
 PROTO_FILES=[
@@ -405,7 +405,11 @@ CC_INCLUDES=[
     'third_party/abseil-cpp',
     'third_party/protobuf',
     'third_party/protobuf/src',
+    'third_party/protobuf/upb_generator/stage0',
+    'third_party/protobuf/upb_generator/cmake',
     'third_party/protobuf/upb',
+    'third_party/protobuf/upb/reflection/stage0',
+    'third_party/protobuf/upb/reflection/cmake',
     'third_party/protobuf/third_party/utf8_range'
 ]
 PROTO_INCLUDE='third_party/protobuf/src'
--- tools/distrib/python/make_grpcio_tools.py
+++ tools/distrib/python/make_grpcio_tools.py
@@ -92,7 +92,10 @@ GRPCIO_TOOLS_ROOT_PREFIX = "tools/distrib/python/grpcio_tools/"
 COPY_FILES_SOURCE_TARGET_PAIRS = [
     ("include", "grpc_root/include"),
     ("src/compiler", "grpc_root/src/compiler"),
-    ("src/core/ext/upb-gen/google/protobuf", "grpc_root/core/ext/upb-gen/google/protobuf"),
+    (
+        "src/core/ext/upb-gen/google/protobuf",
+        "grpc_root/core/ext/upb-gen/google/protobuf",
+    ),
     ("third_party/abseil-cpp/absl", "third_party/abseil-cpp/absl"),
     ("third_party/protobuf/src", "third_party/protobuf/src"),
     ("third_party/protobuf/upb", "third_party/protobuf/upb"),
@@ -187,8 +190,12 @@ def _bazel_name_to_file_path(name):
             # The some files which Bazel generates during the build process,
             # are not accessible for Python build. Therefore, they need to be redirected
             # to use a pre-generated file for Cmake instead.
-            filepath = filepath.replace("/upb/reflection/stage1/", "/upb/reflection/cmake/")
-            filepath = filepath.replace("/upb_generator/stage1/", "/upb_generator/cmake/")
+            filepath = filepath.replace(
+                "/upb/reflection/stage1/", "/upb/reflection/cmake/"
+            )
+            filepath = filepath.replace(
+                "/upb_generator/stage1/", "/upb_generator/cmake/"
+            )
 
             return filepath
     return None
